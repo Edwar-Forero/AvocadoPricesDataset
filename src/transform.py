353a94feb8aa515151da2102249a46e4d69d1764
JSernaOne <johanserna842@gmail.com>
2026-04-21 16:26:01 -0500

    hotfix

--- src/transform.py
+++ src/transform.py
@@ -25,3 +25,14 @@ def add_total_sale(df):
     df["total_amount"] = df["quantity"] * df["price"]
 
     return df
+
+
+def remove_outliers(df, column):
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+
+    lower = Q1 - 1.5 * IQR
+    upper = Q3 + 1.5 * IQR
+
+    return df[(df[column] >= lower) & (df[column] <= upper)]
